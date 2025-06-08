685a5a9b5cfb3bff0b2aa2473985c14b136a0145
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-06-08 21:45:08 +0200

    Also demonstrate top level await

--- t1-b.mjs
+++ t1-b.mjs
@@ -1,2 +1,4 @@
 export const hello = 'world';
-console.log("Loaded module t1-b.mjs");
+console.log("Loaded module t1-b.mjs - now sleeping 1 sec");
+await new Promise(resolve => setTimeout(resolve, 1000));
+
